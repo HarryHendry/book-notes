96b396c29ba478b2de6c5059c5d8a6a929766caa
Harry Hendry <harryhendry89@hotmail.co.uk>
2024-09-19 07:34:59 +0100

    third

--- db.js
+++ db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
